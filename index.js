865906320282e750743982fa12c6502e3d90709e
pappu116 <52205941+pappu116@users.noreply.github.com>
2020-10-03 23:18:23 +0600

    server some compleate

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bfpdn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 5000;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const charitry = client.db("volunterr").collection("items");
+
+  //data posted || addData codes start
+
+  app.post("/addItem", (req, res) => {
+    const newItem = req.body;
+    charitry.insertOne(newItem).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+    console.log(newItem);
+  });
+  //Data posted code End
+
+  //data read code start
+
+  app.get("/items", (req, res) => {
+    charitry.find({ email: req.query.email }).toArray((err, document) => {
+      res.send(document);
+    });
+  });
+
+  //reding code end
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || port);
